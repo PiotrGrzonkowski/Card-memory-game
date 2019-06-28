d9a2af115f724e27abf43ad5ac802ac6745ba970
Piotr <piotr.grzonkowski88@gmail.com>
2019-06-28 13:22:01 +0200

    Updates

---
+++ precache-manifest.8f13e523366ef8f04e3016e395acc98e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1193a1e4e5cde8f8e629fa2e03a53a9e",
+    "url": "/Card-memory-game/index.html"
+  },
+  {
+    "revision": "bff50de01c980688bfeb",
+    "url": "/Card-memory-game/static/css/main.27c49b09.chunk.css"
+  },
+  {
+    "revision": "7a27fe14fa626aa98fc4",
+    "url": "/Card-memory-game/static/js/2.f87c6b8b.chunk.js"
+  },
+  {
+    "revision": "bff50de01c980688bfeb",
+    "url": "/Card-memory-game/static/js/main.8fbae7c8.chunk.js"
+  },
+  {
+    "revision": "bb75e9286096e9d38ab4",
+    "url": "/Card-memory-game/static/js/runtime~main.1f11cd08.js"
+  },
+  {
+    "revision": "d031acc2bbf496e89a3aa03ff0970b36",
+    "url": "/Card-memory-game/static/media/znak zapytania.d031acc2.png"
+  }
+]);
